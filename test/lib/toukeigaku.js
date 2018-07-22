af0970410c3b1c4192199ed809bd8d5db6d01fb0
kjirou <sorenariblog@gmail.com>
2018-07-22 13:22:09 +0900

    相対度数の端数処理を整理

--- test/lib/toukeigaku.js
+++ test/lib/toukeigaku.js
@@ -68,7 +68,93 @@ describe('lib/toukeigaku', function() {
 
       it('最大階級の累積相対度数がおおよそ 1.0 に近い値である', function() {
         const actual = Number(this.度数分布[3].累積相対度数);
-        assert.ok(actual > 0.99 && actual < 1.01);
+        assert.ok(actual > 0.9 && actual < 1.1);
+      });
+    });
+
+    describe('端数処理', function() {
+      describe('切り上げ', function() {
+        describe('小数点第2位までのとき', function() {
+          it('正しい処理である', function() {
+            const 度数分布 = create度数分布([0, 0, 1], 0, 3, 3, {
+              相対度数の丸め桁数: 2,
+              相対度数の丸め手法: '切り上げ',
+            });
+
+            assert.strictEqual(度数分布[0].相対度数, '0.67');
+            assert.strictEqual(度数分布[1].相対度数, '0.34');
+            assert.strictEqual(度数分布[2].相対度数, '0.00');
+          });
+        });
+
+        describe('小数点第0位までのとき', function() {
+          it('正しい処理である', function() {
+            const 度数分布 = create度数分布([0, 0, 1], 0, 3, 3, {
+              相対度数の丸め桁数: 0,
+              相対度数の丸め手法: '切り上げ',
+            });
+
+            assert.strictEqual(度数分布[0].相対度数, '1');
+            assert.strictEqual(度数分布[1].相対度数, '1');
+            assert.strictEqual(度数分布[2].相対度数, '0');
+          });
+        });
+      });
+
+      describe('切り捨て', function() {
+        describe('小数点第2位までのとき', function() {
+          it('正しい処理である', function() {
+            const 度数分布 = create度数分布([0, 0, 1], 0, 3, 3, {
+              相対度数の丸め桁数: 2,
+              相対度数の丸め手法: '切り捨て',
+            });
+
+            assert.strictEqual(度数分布[0].相対度数, '0.66');
+            assert.strictEqual(度数分布[1].相対度数, '0.33');
+            assert.strictEqual(度数分布[2].相対度数, '0.00');
+          });
+        });
+
+        describe('小数点第0位までのとき', function() {
+          it('正しい処理である', function() {
+            const 度数分布 = create度数分布([0, 0, 1], 0, 3, 3, {
+              相対度数の丸め桁数: 0,
+              相対度数の丸め手法: '切り捨て',
+            });
+
+            assert.strictEqual(度数分布[0].相対度数, '0');
+            assert.strictEqual(度数分布[1].相対度数, '0');
+            assert.strictEqual(度数分布[2].相対度数, '0');
+          });
+        });
+      });
+
+      describe('四捨五入', function() {
+        describe('小数点第2位までのとき', function() {
+          it('正しい処理である', function() {
+            const 度数分布 = create度数分布([0, 0, 1], 0, 3, 3, {
+              相対度数の丸め桁数: 2,
+              相対度数の丸め手法: '四捨五入',
+            });
+
+            assert.strictEqual(度数分布[0].相対度数, '0.67');
+            assert.strictEqual(度数分布[1].相対度数, '0.33');
+            assert.strictEqual(度数分布[2].相対度数, '0.00');
+          });
+        });
+
+        describe('小数点第0位までのとき', function() {
+          it('正しい処理である', function() {
+            const 度数分布 = create度数分布([0, 0, 1], 0, 3, 3, {
+              相対度数の丸め桁数: 0,
+              相対度数の丸め手法: '四捨五入',
+            });
+
+            assert.strictEqual(度数分布[0].相対度数, '1');
+            assert.strictEqual(度数分布[1].相対度数, '0');
+            assert.strictEqual(度数分布[2].相対度数, '0');
+          });
+        });
       });
     });
   });
